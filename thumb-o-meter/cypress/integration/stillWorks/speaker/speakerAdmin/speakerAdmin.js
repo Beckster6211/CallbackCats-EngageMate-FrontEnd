78b486c478f472b4ab02033d2169772c94b3defb
= <beckyljones151192@gmail.com>
2021-03-17 17:13:49 +0000

    started test for users table test finds headings and confirms context

--- thumb-o-meter/cypress/integration/stillWorks/speaker/speakerAdmin/speakerAdmin.js
+++ thumb-o-meter/cypress/integration/stillWorks/speaker/speakerAdmin/speakerAdmin.js
@@ -12,8 +12,6 @@ check surname
 check input field type view
 check email
 check input field type speakerview@gmail.com
-
-
 check send app message 
 check send app message drop down 
 click drop down
@@ -61,205 +59,261 @@ function speakerAdmin() {
         .contains("Admin")
         .click();
     });
-    it("Checking the add a user feature", () => {
+    // it("Checking the add a user feature", () => {
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(0)
+    //     .find("button")
+    //     .should("have.id", "accordion-button-5")
+    //     .find("div")
+    //     .should("have.class", "css-1eziwv")
+    //     .contains("Add User Form");
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(0)
+    //     .find("button")
+    //     .should("have.id", "accordion-button-5")
+    //     .eq(0)
+    //     .click();
+    //   //adding user form
+    //   //role
+    //   cy.wait(1000);
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("label")
+    //     .should("have.id", "role-label")
+    //     .contains("Role");
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("div")
+    //     .should("have.class", "chakra-select__wrapper css-42b2qy")
+    //     .find("select")
+    //     .should("have.id", "role")
+    //     .select("Coach")
+    //     .should("have.value", "coach");
+    //   // first name
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(3)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("label")
+    //     .should("have.id", "firstName-label")
+    //     .contains("First name");
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(3)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("input")
+    //     .should("have.id", "firstName")
+    //     .type("Cypress");
+    //   // surname
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(4)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("label")
+    //     .should("have.id", "surname-label")
+    //     .contains("Surname");
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(4)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("input")
+    //     .should("have.id", "surname")
+    //     .type("Testing");
+    //   // email address
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(5)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("label")
+    //     .should("have.id", "email-label")
+    //     .contains("Email address");
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("div")
+    //     .eq(5)
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("input")
+    //     .should("have.id", "email")
+    //     .type("cypressTesting@email.com");
+    //   //add user button
+    //   cy.get("form")
+    //     .should("have.class", "addUserForm_form__1Etim")
+    //     .find("button")
+    //     .should("have.class", "chakra-button css-o3kgci")
+    //     .contains("Add user");
+    //   //close add a user menu
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(0)
+    //     .find("button")
+    //     .should("have.id", "accordion-button-5")
+    //     .eq(0)
+    //     .click();
+    // });
+    // //send message feature
+    // it("Check the send app message feature", () => {
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(10)
+    //     .find("button")
+    //     .should("have.id", "accordion-button-7")
+    //     .find("div")
+    //     .should("have.class", "css-1eziwv")
+    //     .contains("Send App Message");
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(10)
+    //     .find("button")
+    //     .should("have.id", "accordion-button-7")
+    //     .eq(0)
+    //     .click();
+    //   //typeing message
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(10)
+    //     .find("div")
+    //     .should("have.class", "chakra-collapse")
+    //     .find("div")
+    //     .should("have.id", "accordion-panel-7")
+    //     .find("form")
+    //     .find("div")
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("label")
+    //     .should("have.id", "message-label")
+    //     .contains("Enter message:");
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(10)
+    //     .find("div")
+    //     .should("have.class", "chakra-collapse")
+    //     .find("div")
+    //     .should("have.id", "accordion-panel-7")
+    //     .find("form")
+    //     .find("div")
+    //     .should("have.class", "chakra-form-control css-1kxonj9")
+    //     .find("input")
+    //     .should("have.id", "message")
+    //     .type("Cypress message");
+    //   cy.get("main")
+    //     .should("have.class", "admin_main__2iRjR")
+    //     .find("div")
+    //     .eq(0)
+    //     .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+    //     .find("div")
+    //     .should("have.class", "chakra-accordion__item css-17mg6aq")
+    //     .eq(10)
+    //     .find("div")
+    //     .should("have.class", "chakra-collapse")
+    //     .find("div")
+    //     .should("have.id", "accordion-panel-7")
+    //     .find("form")
+    //     .find("button")
+    //     .should("have.class", "chakra-button css-o3kgci")
+    //     .contains("Send")
+    //     .click();
+    // });
+    it("Check looking for users in table", () => {
       cy.get("main")
         .should("have.class", "admin_main__2iRjR")
         .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+        .eq(16)
+        .should("have.class", "chakra-tabs admin_tab__2ytNg css-13o7eu2")
         .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(0)
+        .should("have.class", "chakra-tabs__tablist css-1xhq01z")
         .find("button")
-        .should("have.id", "accordion-button-5")
-        .find("div")
-        .should("have.class", "css-1eziwv")
-        .contains("Add User Form");
+        .should("have.id", "tabs-9--tab-0")
+        .contains("Users");
+      // .click();
       cy.get("main")
         .should("have.class", "admin_main__2iRjR")
         .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
+        .eq(16)
+        .should("have.class", "chakra-tabs admin_tab__2ytNg css-13o7eu2")
         .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(0)
-        .find("button")
-        .should("have.id", "accordion-button-5")
-        .eq(0)
-        .click();
-      //adding user form
-      //role
-      cy.wait(1000);
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
+        .should("have.class", "chakra-tabs__tab-panels css-8atqhb")
         .find("div")
-        .eq(0)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("label")
-        .should("have.id", "role-label")
-        .contains("Role");
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
+        .should("have.id", "tabs-9--tabpanel-0")
         .find("div")
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("div")
-        .should("have.class", "chakra-select__wrapper css-42b2qy")
-        .find("select")
-        .should("have.id", "role")
-        .select("Coach")
-        .should("have.value", "coach");
-      // first name
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(3)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("label")
-        .should("have.id", "firstName-label")
-        .contains("First name");
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(3)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("input")
-        .should("have.id", "firstName")
-        .type("Cypress");
-      // surname
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(4)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("label")
-        .should("have.id", "surname-label")
-        .contains("Surname");
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(4)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("input")
-        .should("have.id", "surname")
-        .type("Testing");
-      // email address
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(5)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("label")
-        .should("have.id", "email-label")
-        .contains("Email address");
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("div")
-        .eq(5)
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("input")
-        .should("have.id", "email")
-        .type("cypressTesting@email.com");
-      //add user button
-      cy.get("form")
-        .should("have.class", "addUserForm_form__1Etim")
-        .find("button")
-        .should("have.class", "chakra-button css-o3kgci")
-        .contains("Add user");
-      //close add a user menu
+        .should("have.class", "admin_userTable__2uCq4")
+        .find("table")
+        .should("have.class", "chakra-table css-1ac7ejl")
+        .find("caption")
+        .should("have.class", "css-gdv02")
+        .contains("Registered users");
+      //check table headings
       cy.get("main")
         .should("have.class", "admin_main__2iRjR")
         .find("div")
+        .eq(16)
+        .should("have.class", "chakra-tabs admin_tab__2ytNg css-13o7eu2")
+        .find("div")
+        .should("have.class", "chakra-tabs__tab-panels css-8atqhb")
+        .find("div")
+        .should("have.id", "tabs-9--tabpanel-0")
+        .find("div")
+        .should("have.class", "admin_userTable__2uCq4")
+        .find("table")
+        .should("have.class", "chakra-table css-1ac7ejl")
+        .find("thead")
+        .should("have.class", "css-0")
+        .find("tr")
+        .should("have.class", "css-0")
         .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(0)
-        .find("button")
-        .should("have.id", "accordion-button-5")
-        .eq(0)
-        .click();
-    });
-    it("Check the send app message feature", () => {
-      cy.get("main")
-        .should("have.class", "admin_main__2iRjR")
-        .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(10)
-        .find("button")
-        .should("have.id", "accordion-button-7")
-        .find("div")
-        .should("have.class", "css-1eziwv")
-        .contains("Send App Message");
-      cy.get("main")
-        .should("have.class", "admin_main__2iRjR")
-        .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(10)
-        .find("button")
-        .should("have.id", "accordion-button-7")
-        .eq(0)
-        .click();
-      //typeing message
-      cy.get("main")
-        .should("have.class", "admin_main__2iRjR")
-        .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(10)
-        .find("div")
-        .should("have.class", "chakra-collapse")
-        .find("div")
-        .should("have.id", "accordion-panel-7")
-        .find("form")
-        .find("div")
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("label")
-        .should("have.id", "message-label")
-        .contains("Enter message:");
-      cy.get("main")
-        .should("have.class", "admin_main__2iRjR")
-        .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(10)
-        .find("div")
-        .should("have.class", "chakra-collapse")
-        .find("div")
-        .should("have.id", "accordion-panel-7")
-        .find("form")
-        .find("div")
-        .should("have.class", "chakra-form-control css-1kxonj9")
-        .find("input")
-        .should("have.id", "message")
-        .type("Cypress message");
-      cy.get("main")
-        .should("have.class", "admin_main__2iRjR")
-        .find("div")
-        .eq(0)
-        .should("have.class", "chakra-accordion admin_accordion__1C9yH css-0")
-        .find("div")
-        .should("have.class", "chakra-accordion__item css-17mg6aq")
-        .eq(10)
-        .find("div")
-        .should("have.class", "chakra-collapse")
-        .find("div")
-        .should("have.id", "accordion-panel-7")
-        .find("form")
-        .find("button")
-        .should("have.class", "chakra-button css-o3kgci")
-        .contains("Send")
-        .click();
-      cy.wait(2000);
+        .within(() => {
+          cy.get("th").eq(1).contains("First name");
+          cy.get("th").eq(2).contains("Surname");
+          cy.get("th").eq(3).contains("Role");
+          cy.get("th").eq(4).contains("Bootcamper Id");
+          cy.get("th").eq(5).contains("Cohort no");
+          cy.get("th").eq(6).contains("Email");
+        });
     });
   });
 }
@@ -267,5 +321,22 @@ function speakerAdmin() {
 export default speakerAdmin;
 
 /*
+<table>
+  <tr>
+    <td>My first client</td>
+    <td>My first project</td>
+    <td>0</td>
+    <td>Active</td>
+    <td><button>Edit</button></td>
+  </tr>
+</table>
 
+
+cy.contains('My first client').parent('tr').within(() => {
+  // all searches are automatically rooted to the found tr element
+  cy.get('td').eq(1).contains('My first project')
+  cy.get('td').eq(2).contains('0')
+  cy.get('td').eq(3).contains('Active')
+  cy.get('td').eq(4).contains('button', 'Edit').click()
+})
 */
