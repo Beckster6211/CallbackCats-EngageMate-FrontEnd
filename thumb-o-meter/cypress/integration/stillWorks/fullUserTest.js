3918674ecb80597e77a7e151ae23e2c06602626f
= <beckyljones151192@gmail.com>
2021-03-18 12:57:04 +0000

    refactored darkmode and light mode

--- thumb-o-meter/cypress/integration/stillWorks/fullUserTest.js
+++ thumb-o-meter/cypress/integration/stillWorks/fullUserTest.js
@@ -1,12 +1,17 @@
+/*
+opening screen 
+log in 
+dark mode
+light mode
+
+*/
+
 import openingScreen from "./openingScreen/openingScreen";
 import logIn from "./logIn/logIn";
 import darkMode from "./darkMode/darkMode";
 import lightMode from "./lightMode/lightMode";
-import speakerAdmin from "./speaker/speakerAdmin/speakerAdmin";
 
 openingScreen();
 logIn("speakerview@gmail.com", "(callbackCats)");
 darkMode();
-// lightMode();
-// darkMode();
-speakerAdmin();
+lightMode();
